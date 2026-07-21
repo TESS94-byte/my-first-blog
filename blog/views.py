ea7b08bf6c87eaa7320cab2e6126b789655a9cf0
TESS94-byte <tzaouibensalem@ivytech.edu>
2026-07-20 22:55:01 -0400

    Complete Django tutorial CSS templates and forms

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -9,3 +10,19 @@ def post_list(request):
     ).order_by('published_date')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_edit.html', {'form': form})
