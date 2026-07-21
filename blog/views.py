71e222df55dbd8dd8e73715cca965929dc8870f2
TESS94-byte <tzaouibensalem@ivytech.edu>
2026-07-20 23:29:37 -0400

    Completed Django Girls CSS templates and forms sections

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -12,6 +12,12 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -19,8 +25,10 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.published_date = timezone.now()
             post.save()
-            return redirect('post_list')
+
+            return redirect('post_detail', pk=post.pk)
 
     else:
         form = PostForm()
